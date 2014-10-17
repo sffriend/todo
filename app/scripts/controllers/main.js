84a07a6de8e618e38236f3bf27620e5aa25b4f9b
Stephanie <stephanie.f.friend@gmail.com>
2014-10-16 23:51:12 -0700

    working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,13 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name todoApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the todoApp
- */
 angular.module('todoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
   });
