fbc3bd8f29368a56b49c86870937119d3f693ad4
Stephanie <stephanie.f.friend@gmail.com>
2014-10-18 12:06:46 -0700

    small changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,9 +23,9 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .when('/tasks', {
+        templateUrl: 'views/tasks.html',
+        controller: 'TasksCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,7 +8,7 @@
  * Controller of the todoApp
  */
 angular.module('todoApp')
-  .controller('AboutCtrl', function ($scope) {
+  .controller('TasksCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
