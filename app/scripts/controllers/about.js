7c960a096be70fdc10595db26b422dbdb000fd4c
carllindiii <carl.lind.iii@gmail.com>
2014-11-10 19:19:53 -0800

    reformatting code

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,10 @@
  * Controller of the todoApp
  */
 angular.module('todoApp')
-  .controller('TasksCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+   .controller('TasksCtrl', function ($scope) {
+      $scope.awesomeThings = [
+         'HTML5 Boilerplate',
+         'AngularJS',
+         'Karma'
+      ];
+   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,12 +9,12 @@
  * @param done [boolean] completed task
  * @param priority [
  */
-var todo = function(name, project, date, priority, isDone) {
-    this.name = name;
-    this.project = project;
-    this.date = date;
-    this.priority = priority;
-    this.isDone = isDone;
+var todo = function (name, project, date, priority, isDone) {
+   this.name = name;
+   this.project = project;
+   this.date = date;
+   this.priority = priority;
+   this.isDone = isDone;
 };
 
 
@@ -24,71 +24,70 @@ var todo = function(name, project, date, priority, isDone) {
  * @type {{NONE: string, LOW: string, MEDIUM: string, HIGH: string}}
  */
 var priorityEnum = {
-    AUTO : 'AUTO',
-    LOW : 'LOW',
-    MEDIUM : 'MEDIUM',
-    HIGH : 'HIGH'
+   AUTO: 'AUTO',
+   LOW: 'LOW',
+   MEDIUM: 'MEDIUM',
+   HIGH: 'HIGH'
 }
 
 
 angular.module('todoApp')
-  .controller('MainCtrl', function ($scope, $cookies) {
+   .controller('MainCtrl', function ($scope, $cookies) {
 
-    $scope.todos = [];
-    $scope.date = "None";
+      $scope.todos = [];
+      $scope.date = "None";
 
-    // if todos exist in cookies, add them to the page
-    if($cookies != undefined && $cookies.todos != undefined) {
-        var todos = JSON.parse($cookies.todos);
-        for (var i = 0; i < todos.length; i++) {
+      // if todos exist in cookies, add them to the page
+      if ($cookies != undefined && $cookies.todos != undefined) {
+         var todos = JSON.parse($cookies.todos);
+         for (var i = 0; i < todos.length; i++) {
             $scope.todos.push(todos[i]);
-        }
-    }
-
-    // adding a task
-    $scope.addTodo = function () {
-      console.log($scope.priority);
-
-      if ($scope.priority == priorityEnum.AUTO) {
-        var milToDays = 1.157e8
-        var time = Math.abs((new Date() - new Date($scope.date)) / milToDays);
-
-        if (time <= 5) {
-          $scope.priority = priorityEnum.HIGH;
-        }
-        else if (time > 5 && time <= 20) {
-          $scope.priority = priorityEnum.MEDIUM;
-        }
-        else if (time > 20) {
-          $scope.priority = priorityEnum.LOW;
-        }
+         }
       }
 
-      var newTodo = new todo($scope.name, $scope.project, $scope.date, $scope.priority, false);
-      $scope.todos.push(newTodo);
-      //reload cookies for todos
-      $cookies.todos = JSON.stringify($scope.todos);
-      clearInput($scope);
-    };
+      // adding a task
+      $scope.addTodo = function () {
+         console.log($scope.priority);
 
-    // removing a task
-    $scope.removeTodo = function (t, index) {
-        //t.addClass('remove');
+         if ($scope.priority == priorityEnum.AUTO) {
+            var milToDays = 1.157e8
+            var time = Math.abs((new Date() - new Date($scope.date)) / milToDays);
 
-      $scope.todos.splice(index, 1);
-      // overwrite the tasks saved in cookies
-      $cookies.todos = JSON.stringify($scope.todos);
-    };
+            if (time <= 5) {
+               $scope.priority = priorityEnum.HIGH;
+            }
+            else if (time > 5 && time <= 20) {
+               $scope.priority = priorityEnum.MEDIUM;
+            }
+            else if (time > 20) {
+               $scope.priority = priorityEnum.LOW;
+            }
+         }
 
-     // removing a task
-    $scope.editTodo = function (index) {
-        // overwrite the tasks saved in cookies
-      $cookies.todos = JSON.stringify($scope.todos);
-    };
+         var newTodo = new todo($scope.name, $scope.project, $scope.date, $scope.priority, false);
+         $scope.todos.push(newTodo);
+         //reload cookies for todos
+         $cookies.todos = JSON.stringify($scope.todos);
+         clearInput($scope);
+      };
+
+      // removing a task
+      $scope.removeTodo = function (t, index) {
+         //t.addClass('remove');
+
+         $scope.todos.splice(index, 1);
+         // overwrite the tasks saved in cookies
+         $cookies.todos = JSON.stringify($scope.todos);
+      };
+
+      // removing a task
+      $scope.editTodo = function (index) {
+         // overwrite the tasks saved in cookies
+         $cookies.todos = JSON.stringify($scope.todos);
+      };
 
 
-
-  });
+   });
 
 
 /**
@@ -96,10 +95,11 @@ angular.module('todoApp')
  * @returns {Date}
  */
 function convertDate() {
-return null;}
+   return null;
+}
 
 function createPriority() {
-  return 'MEDIUM';
+   return 'MEDIUM';
 }
 
 /**
@@ -107,30 +107,30 @@ function createPriority() {
  * @param $scope
  */
 function clearInput($scope) {
-    $scope.name = '';
-    $scope.description = '';
-    $scope.date = '';
-    $scope.project = '';
-    $scope.priority = 'AUTO';
-    $(this).closest('.add-todo').hide();
+   $scope.name = '';
+   $scope.description = '';
+   $scope.date = '';
+   $scope.project = '';
+   $scope.priority = 'AUTO';
+   $(this).closest('.add-todo').hide();
 }
 
-angular.module('todoApp').directive('datepicker', function() {
-    return {
-        restrict: 'A',
-        require : 'ngModel',
-        link : function (scope, element, attrs, ngModelCtrl) {
-            $(function(){
-                element.datepicker({
-                    dateFormat:'mm/dd/yy',
-                    onSelect:function (date) {
-                        ngModelCtrl.$setViewValue(date);
-                        scope.$apply();
-                    }
-                });
+angular.module('todoApp').directive('datepicker', function () {
+   return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function (scope, element, attrs, ngModelCtrl) {
+         $(function () {
+            element.datepicker({
+               dateFormat: 'mm/dd/yy',
+               onSelect: function (date) {
+                  ngModelCtrl.$setViewValue(date);
+                  scope.$apply();
+               }
             });
-        }
-    }
+         });
+      }
+   }
 });
 
 
